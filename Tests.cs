364924ee321718dd34ea3c2dbb1139a50191e595
Viktor Losiev <viktorlosiev@gmail.com>
2025-12-20 20:37:49 +0200

    Added example of desktop ui tests.

---
+++ Tests.cs
@@ -0,0 +1,37 @@
+﻿using FlaUI.Core;
+using FlaUI.Core.AutomationElements;
+using FlaUI.UIA3;
+
+namespace DesktopUiTests
+{
+    public class Tests
+    {
+        private Application _app;
+
+        [SetUp]
+        public void Setup()
+        {
+             _app = Application.Launch("C:\\repository\\DesktopUiTests\\Application\\DataBindingDemo.exe");
+
+        }
+
+        [Test]
+        public void Test()
+        {
+            using (var automation = new UIA3Automation())
+            {
+                var window = _app.GetMainWindow(automation);
+                var button1 = window.FindFirstDescendant(cf => cf.ByText("1"))?.AsButton();
+                button1?.Invoke();
+            }           
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            _app.Dispose();
+            _app.Kill();
+
+        }
+    }
+}
